d620588cc51d8e708e1ee7942e5e1cb8a1b113c9
Alin Georgian Antoci <brinza.alin.georgian@gmail.com>
2017-03-21 16:25:05 +0200

    Distanta noduri - unsolved

--- Ex. 1/Ex. 1/Source.cpp
+++ Ex. 1/Ex. 1/Source.cpp
@@ -54,6 +54,7 @@ void insertFirst(List*& lst, Node* node) {
 			node->next = lst->first;
 			lst->first->prev = node;
 			lst->first = node;
+
 		}
 		else {
 			printf("The list already contains this value (%d). \n\n", node->info);
@@ -76,21 +77,89 @@ void printList(List* lst) {
 }
 
 
+/*
+1. b) O funcție care să determine distanța dintre elementele care conțin valoarea maximă și minimă din listă.
+*/
+void min_max(List* lst, int&min, int&max) {
+	List* tmp = lst;
+
+	min = INT_MAX;
+	max = INT_MIN;
+	bool bool_min = false;
+	bool bool_max = false;
+
+	int d = 0;
+
+	while (tmp->first != NULL) {
+		if (tmp->first->info < min) {
+			min = tmp->first->info;
+			bool_min = true;
+			if(bool_max==true)
+			{
+				d++;
+			}
+			else d = 0;
+		}
+		
+		if (tmp->first->info > max) {
+			max = tmp->first->info;
+			bool_max = true;
+			if (bool_min == true)
+			{
+				d++;
+			}
+			else d = 0;
+		}
+		
+		tmp->first = tmp->first->next;
+	}
+	d--;
+	printf("min: %d, max: %d \n", min, max);
+	printf("distanta:  %d. \n", d);
+
+	
+}
+
+//int distance(List* lst, int min, int max)
+//{
+//	int i = 0;
+//	
+//	while (lst->first != NULL) {
+//		if (lst->first->info == min) {
+//			while (lst->first->info != max) {
+//				i=i+1;
+//				lst->first = lst->first->next;
+//			}
+//		}
+//		lst->first = lst->first->next;
+//	}
+//
+//	return i;
+//}
 
 void main() {
+	int min, max;
 	List* lst = initializeList();
 
-	Node* n = createNode(1);
+	Node* n = createNode(11);
 	insertFirst(lst, n);
 
 	n = createNode(2);
 	insertFirst(lst, n);
 
-	n = createNode(3);
+	n = createNode(10);
 	insertFirst(lst, n);
 
 	n = createNode(3);
 	insertFirst(lst, n);
+
+	//n = createNode(3);
+	//insertFirst(lst, n);
 
 	printList(lst);
+
+	min_max(lst, min, max);
+
+	//int distanta = distance(lst, min, max);
+	
 }

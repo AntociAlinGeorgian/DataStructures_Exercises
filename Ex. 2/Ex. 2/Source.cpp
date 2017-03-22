68ccf69a25be7bf3aa204145b9ece64c98762d74
Alin Georgian Antoci <brinza.alin.georgian@gmail.com>
2017-03-22 16:16:38 +0200

    Exercise 2.

---
+++ Ex. 2/Ex. 2/Source.cpp
@@ -0,0 +1,7 @@
+﻿/*
+a) O funcție pentru inserarea unei date într-o listă liniară simplu înlănțuită care să permită gruparea
+valorilor impare și pare. Valorile pare sunt inserate în prima jumătate la începutul liste iar cele impare
+în a doua jumătate la sfârșitul listei. Informația utilă a elementelor listei este de tip întreg.
+b) O funcție care să determine numărul de elemente pare și impare din listă.
+*/
+

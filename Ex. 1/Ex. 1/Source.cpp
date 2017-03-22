20702dcdc8843caee7a1f3b2444e6359d2cba20f
Alin Georgian Antoci <brinza.alin.georgian@gmail.com>
2017-03-22 16:02:39 +0200

    Added  the problem requirement.

--- Ex. 1/Ex. 1/Source.cpp
+++ Ex. 1/Ex. 1/Source.cpp
@@ -1,4 +1,11 @@
-﻿#include<iostream>
+﻿/*
+1. a) O funcție pentru inserarea unei date într-o listă liniară dublu înlănțuită care să nu permită valori
+duplicate (dacă valoarea nu există, inserarea se realizează la începutul listei). Informația utilă a
+elementelor listei este de tip întreg.
+b) O funcție care să determine distanța dintre elementele care conțin valoarea maximă și minimă din listă.
+*/
+
+#include<iostream>
 #include<stdio.h>
 
 

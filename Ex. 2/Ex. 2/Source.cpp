433ce5d63d1fdea1f25b10ae585fa78b44958229
Alin Georgian Antoci <brinza.alin.georgian@gmail.com>
2017-03-27 12:29:56 +0300

    added Readme

--- Ex. 2/Ex. 2/Source.cpp
+++ Ex. 2/Ex. 2/Source.cpp
@@ -5,3 +5,63 @@ valorilor impare și pare. Valorile pare sunt inserate în prima jumătate la î
 b) O funcție care să determine numărul de elemente pare și impare din listă.
 */
 
+#include<iostream>
+#include<stdio.h>
+
+struct Node {
+	int info;
+	Node* next;
+};
+
+Node* createNode(int nr) {
+	Node* nod = new Node;
+	nod->info = nr;
+	nod->next = NULL;
+	return nod;
+}
+
+void insertNode(Node*& lst, Node* nod) {
+	if (lst==NULL) {
+		lst = nod;
+	}
+	else {
+		if (nod->info % 2 == 0) {
+			nod->next = lst;
+			lst = nod;
+		}
+		else if (nod->info % 2 == 1) {
+			Node* tmp = lst;
+			while (tmp->next != NULL) {
+				tmp = tmp->next;
+			}
+			tmp->next = nod;
+		}
+	}
+}
+
+void printList(Node* lst) {
+	if (lst) {
+		Node* tmp = lst;
+		while (tmp != NULL) {
+			printf("%d \n", tmp->info);
+			tmp = tmp->next;
+		}
+	}
+}
+
+
+void readFromFile(Node*& lst) {
+	FILE* f = fopen("Text.txt", "r");
+	if (f) {
+		while (!feof(f)) {
+			Node* n = createNode(fscanf(f, "%d"));
+			insertNode(lst, n);
+		}
+	}
+}
+
+void main() {
+	Node* lst = nullptr;
+	readFromFile(lst);
+	printList(lst);
+}
